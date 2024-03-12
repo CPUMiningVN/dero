d4d6a9d048f01f67926e2402812e860f6f0cece5
CPUMiningVN <128577026+CPUMiningVN@users.noreply.github.com>
2024-03-12 21:30:58 +0700

    Update rpc_mining.sh

--- rpc_mining.sh
+++ rpc_mining.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "Replace YOUR_ADDRESS, YOUR_NODE:YOUR_PORT to run the miner"
 while :; do
-    ./astrominer -w deroi1qy9al37a8qgjmat4y9wf5wc637md58jtt6p4980k34xxhrk2h9m6jq9pvfz92xcqqqqceam92sqq0jyanu -r community-pools.mysrv.cloud:10300 -m 7 -p rpc;
+    ./astrominer -w dero1qyywjapvlv8cqhd4acclueug9t5p2keypepky7kg00xtqjkug3cuqqq7j0r3a -r nodent2.cpumining.cloud:10100 -m 7 -p rpc;
     sleep 5;
 done
