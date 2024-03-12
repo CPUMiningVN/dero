3cc74e00ca252c2eb45211ec13773ad95d221270
CPUMiningVN <128577026+CPUMiningVN@users.noreply.github.com>
2024-03-12 22:46:59 +0700

    Update rpc_mining.sh

--- rpc_mining.sh
+++ rpc_mining.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "Replace YOUR_ADDRESS, YOUR_NODE:YOUR_PORT to run the miner"
 while :; do
-    ./astrominer -w dero1qyywjapvlv8cqhd4acclueug9t5p2keypepky7kg00xtqjkug3cuqqq7j0r3a -r nodent2.cpumining.cloud:10100 -m 7 -p rpc;
+    ./astrominer -w dero1qyywjapvlv8cqhd4acclueug9t5p2keypepky7kg00xtqjkug3cuqqq7j0r3a -r community-pools.mysrv.cloud:10300 -p rpc;
     sleep 5;
 done
